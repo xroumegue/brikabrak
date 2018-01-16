a3b4b9b4c0fa1d9f3bc4b77da421b87e2127bf0e
Antoine Bouyer <an2ane.bouyer@gmail.com>
2018-01-16 11:41:43 +0100

    bin: chrootBuddy: add ubuntu support
    Signed-off-by: Antoine Bouyer <an2ane.bouyer@gmail.com>

--- bin/chrootBuddy.sh
+++ bin/chrootBuddy.sh
@@ -10,6 +10,7 @@ RELEASE="testing"
 ARCH="amd64"
 USER="root"
 GROUP="chroot"
+SYSTEM="debian"
 
 me=$(readlink -f "$0" | cut -d \. -f 1)
 share=$(echo $me | sed -e "s/bin/share/g")
@@ -27,6 +28,7 @@ while true; do
 		-P | --packages) PACKAGES_LIST+=" $2"; shift 2;;
 		-e | --extra) EXTRA_LIST=$2; shift 2;;
 		-g | --group) GROUP=$2; shift 2;;
+		-s | --system) SYSTEM=$2; shift 2;;
 		-- ) shift; break;;
 		*) break ;;
 	esac
@@ -41,7 +43,7 @@ function log {
 function help {
 	cat <<EOF
 NAME:
-	$0 - Generate a debian chroot with pre-installed packages
+	$0 - Generate a debian or ubuntu chroot with pre-installed packages
 
 OPTIONS:
 	--verbose: Generate more logs
@@ -50,10 +52,12 @@ OPTIONS:
 	--user: user authorized to use the chroot
 	--group: group authorized to use the chroot
 	--release: Debian release (oldstable, stable, testing, unstable)
+		   Ubuntu release (17.10, 16.04, 14.04))
 	--arch: chroot architecture
 	--prefix: Where to install the chroot
 	--extra: postinstalled packages, at next available version
 	--packages: preinstalled packages (see $packages)
+	--system: ubuntu|debian
 
 EXAMPLE:
 	sudo ./chrootBuddy.sh  --verbose --name enet --release stable --prefix /srv/chroot --arch amd64 --extra cuda --packages enet
@@ -67,13 +71,13 @@ then
 	help
 fi
 
-
-
 case $RELEASE in
 	"oldstable") EXTRA_RELEASE="stable";;
 	"stable") EXTRA_RELEASE="testing";;
 	"testing") EXTRA_RELEASE="unstable";;
 	"unstable") EXTRA_RELEASE="experimental";;
+	"16.04") RELEASE="xenial"; EXTRA_RELEASE="artful";;
+	"14.04") RELEASE="trusty"; EXTRA_RELEASE="xenial";;
 	*) echo 'Error while defining the distribution'; exit 1;;
 esac
 
@@ -84,7 +88,6 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 CHROOT_ROOT="$PREFIX"
-MIRROR=http://ftp.fr.debian.org/debian/
 CHROOT_NAME=$NAME
 CHROOT_HOME=$CHROOT_ROOT/$CHROOT_NAME
 CHROOT_USER=$USER
@@ -110,6 +113,17 @@ fi
 
 SCRIPT_2ND_STAGE=/root/2ndstage_$CHROOT_NAME
 
+case $SYSTEM in
+	"debian") MIRROR=http://ftp.fr.debian.org/debian/;;
+	"ubuntu") MIRROR=http://archive.ubuntu.com/ubuntu;;
+	*) echo "Unsupported system $SYSTEM"; exit 1;;
+esac
+
+echo debootstrap --include="$PACKAGES" --variant=buildd --arch="$ARCH" "$RELEASE" "$CHROOT_HOME" "$MIRROR"
+read -p "Here is debootstrap command, is that ok (y/N) ?" resp
+
+[ "$resp" != "y" ] && echo "fine .. maybe next time" && exit 1;
+
 set -x
 debootstrap --include="$PACKAGES" --arch="$ARCH" "$RELEASE" "$CHROOT_HOME" "$MIRROR"
 
@@ -122,7 +136,7 @@ if [ ! -f "$CHROOT_CONF"  ]; then
 log "Creating dedicated schroot conf file $CHROOT_CONF "
 cat > "$CHROOT_CONF" <<EOF
 [$CHROOT_NAME]
-description=Debian $RELEASE $ARCH
+description=$SYSTEM $RELEASE $ARCH
 directory=$CHROOT_HOME
 preserve-environment=true
 root-groups=root
@@ -146,10 +160,16 @@ fi
 log "Configuring fstab for shm"
 sed -i  -e 's/#\/dev\/shm/\/dev\/shm/' "/etc/schroot/$NAME/fstab"
 
-log "Configuring release sources... "
+if [ $SYSTEM = "debian" ]; then
+	log "Configuring release sources... "
 
-{ echo "deb $MIRROR $RELEASE main contrib non-free";echo "deb-src $MIRROR $RELEASE main contrib non-free"; echo "deb $MIRROR $EXTRA_RELEASE main contrib non-free"; echo "deb-src $MIRROR $EXTRA_RELEASE main contrib non-free";} > "$CHROOT_HOME/etc/apt/sources.list"
+{ echo "deb $MIRROR $RELEASE main contrib non-free"; echo "deb-src $MIRROR $RELEASE main contrib non-free"; echo "deb $MIRROR $EXTRA_RELEASE main contrib non-free"; echo "deb-src $MIRROR $EXTRA_RELEASE main contrib non-free";} > "$CHROOT_HOME/etc/apt/sources.list"
 
+else
+
+{ echo "deb $MIRROR $RELEASE main restricted"; echo "deb $MIRROR $RELEASE-updates main restricted"; echo "deb $MIRROR $RELEASE universe"; echo "deb $MIRROR $RELEASE-updates universe"; echo "deb $MIRROR $RELEASE multiverse"; echo "deb $MIRROR $RELEASE-updates multiverse";} > "$CHROOT_HOME/etc/apt/sources.list"
+
+fi
 
 log "Configuring packets pinning... "
 cat > "$CHROOT_HOME/etc/apt/preferences" <<EOF
@@ -157,12 +177,12 @@ Package: *
 Pin: release a=$RELEASE
 Pin-Priority: 800
 
-
 Package: *
 Pin: release a=$EXTRA_RELEASE
 Pin-Priority: 500
 
 EOF
+
 
 if [ "${http_proxy:-unset}" != 'unset' ]; then
 log "Configuring http proxy ... "
